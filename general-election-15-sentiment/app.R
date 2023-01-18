b0ea7b731ef83a2fecdafbba26593eac27ed4512
Aniq Aqil <niqaqil13579@gmail.com>
2023-01-18 17:08:04 +0800

    update app.R

--- general-election-15-sentiment/app.R
+++ general-election-15-sentiment/app.R
@@ -1,50 +1,150 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+#import library
 library(shiny)
+library(shinydashboard)
+library(readr)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- dashboardPage(
+  dashboardHeader(title = "General Election 15"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Overview", tabName = "overview", icon = icon("map-o")),
+      menuItem("GE15 Sentiment Analysis", tabName = "ge15", icon = icon("quote-right")),
+      menuItem("Party Leader", tabName = "party", icon = icon("users")),
+      menuItem("Data", tabName = "data", icon = icon("database")),
+      menuItem("About this site", tabName = "about", icon = icon("info"))
     )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "overview",
+        h3("Overview of Malaysia General Election 15"),
+        fluidRow(
+          valueBox(222, "Parliments", icon = icon("map-marker"), color = "teal"),
+          valueBox(946, "Candidates", icon = icon("user"), color = "teal")
+        ),
+        h4("Result of Parliment"),
+        fluidRow(
+          box(
+            title = "PH", "76 seats", width = 2, height = 150,
+            status = "info", solidHeader = TRUE#, imageOutput("ph_img")
+          ),
+          box(
+            title = "PN", "52 seats", width = 2, height = 150,
+            status = "info", solidHeader = TRUE#, imageOutput("pn_img")
+          ),
+          box(
+            title = "BN", "30 seats", width = 2, height = 150,
+            status = "info", solidHeader = TRUE#, imageOutput("bn_img")
+          ),
+          box(
+            title = "GPS", "23 seats", width = 2, height = 150, 
+            status = "info", solidHeader = TRUE#, imageOutput("gps_img")
+          ),
+          box(
+            title = "PAS", "22 seats", width = 2, height = 150,
+            status = "info", solidHeader = TRUE#, imageOutput("pas_img")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "GRS", "6 seats", width = 2, height = 150, 
+            status = "info", solidHeader = TRUE#, imageOutput("grs_img")
+          ),
+          box(
+            title = "DAP", "5 seats", width = 2, height = 150,
+            status = "info", solidHeader = TRUE#, imageOutput("dap_img")
+          ),
+          box(
+            title = "WARISAN", "3 seats", width = 2, height = 150, 
+            status = "info", solidHeader = TRUE#, imageOutput("warisan_img")
+          ),
+          box(
+            title = "BEBAS", "2 seats", width = 2, height = 150,
+            status = "info", solidHeader = TRUE#, imageOutput("bbs_img")
+          ),
+          box(
+            title = "MUDA", "1 seat", width = 2, height = 150, 
+            status = "info", solidHeader = TRUE#, imageOutput("muda_img")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "ge15",
+        h2("Sentiment Analysis of GE15"),
+        fluidRow(
+          box(
+            title = "Input", width = 3, status = "primary", solidHeader = TRUE, collapsible = TRUE,
+            selectInput("time", "Select timeframe", c("Pre-election", "during election", "Post-election"), selected = "during election")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Frequency of tweets", background = "navy",
+            #plotOutput("frequency_plot")
+          ),
+          box(
+            title = "Word Cloud about #GE15", background = "aqua",
+            # plotOutput("word_cloud")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Distribution of sentiment score", background = "teal", 
+            #plotOutput("distribute_score")
+          ),
+          box(
+            title = "Sentiment tweets", background = "blue",
+            #plotOutput("sentiment_bar")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "party",
+        h2("Leader of Party"), 
+        h4("This analysis is focused on three main party which is Pakatan Harapan, Perikatan Nasional, and Barisan Nasional"),
+        fluidRow(
+          box(
+            title = "Input", width = 3, status = "success", solidHeader = TRUE, collapsible = TRUE,
+            selectInput("leader", "Select leader", c("Anwar Ibrahim (PH)", "Muhyiddin Yassin (PN)", "Zahid Hamidi (BN)"))
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Frequency of tweets", background = "green",
+            #plotOutput("frequency_plot_party")
+          ),
+          box(
+            title = "Word Cloud", background = "lime",
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Distribution of sentiment score", background = "olive",
+            #plotOutput("distribute_score_party")
+          ),
+          box(
+            title = "Sentiment tweets", background = "green",
+            #plotOutput("sentiment_bar_party")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "data"
+      ),
+      tabItem(
+        tabName = "about",
+        h3("About this project")
+      )
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    
 }
 
 # Run the application 
